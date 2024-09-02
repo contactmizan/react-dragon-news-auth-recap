824701edcefa7784a2547ff898452d85dd53c916
contactmizan <contactmizan1206@gmail.com>
2024-09-03 00:25:19 +0600

    done authentication work

---
+++ src/providers/Authprovider.jsx
@@ -0,0 +1,49 @@
+import { createContext, useEffect, useState } from "react";
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import app from "../firebase/firebase.config";
+
+export const AuthContext = createContext(null);
+
+const auth = getAuth(app);
+
+const Authprovider = ({ children }) => {
+
+    const [user, setUser] = useState(null);
+
+    const createUser = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    const signIn = (email, password) =>{
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    const logOut = () => {
+        return signOut(auth);
+    }
+
+    useEffect(() => {
+        const unSubscribe = onAuthStateChanged(auth, currentUser => {
+            console.log('user in the auth state changed', currentUser);
+            setUser(currentUser)
+        });
+        return () => {
+            unSubscribe();
+        }
+    }, [])
+
+    const authinfo = {
+        user,
+        createUser,
+        signIn,
+        logOut
+    }
+
+    return (
+        <AuthContext.Provider value={authinfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+}
+
+export default Authprovider;
